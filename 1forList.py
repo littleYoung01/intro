950cb61fdd9a783ecc0db5c2509f53e6a3d27626
Yuyang <Yuyang_Zhao@foxmail.com>
2022-03-10 19:33:38 +0800

    Make a for item in iterable loop that prints each item in the list

---
+++ 1forList.py
@@ -0,0 +1,8 @@
+# Make a list.
+list=['I love Python','I love Java','I love making games','I love Math']
+
+# Make a for item in iterable loop that prints each item in the list
+for show in list:
+    print(show)
+
+
